dd7c0d14affb4e380c736bb92512eea55c3f1fef
Alexandre Jacobs <jacalex@live.be>
2016-11-02 18:24:51 +0100

    Add files via upload
    Début de code de base des différentes structures qu'on pourrait utiliser. A  en discuter.

---
+++ Graph.py
@@ -0,0 +1,22 @@
+class Graph:
+    def __int__(self, size):
+        self.nodes = dict()
+        self.order = size
+
+    def read_file(self,file_name):
+        pass
+
+    def add_node(self, node):
+        pass
+
+    def get_node(self, node):
+        pass
+
+    def add_node_from_file(self, read):
+        pass
+
+    def add_loan_from_file(self, read):
+        pass
+
+    #TODO faire l'implémentation des algorithmes qui sont demandée et ajouter ceux au choix.
+
