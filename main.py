bfa6d1d403ef1a343c810ba7878304c433e23fff
AlejacobULB <alejacob@ulb.ac.be>
2016-11-29 11:25:40 +0100

    Ajout des output pour les algo et exécution sur différent fichiers data.

--- main.py
+++ main.py
@@ -4,29 +4,28 @@ from graph import DirectedGraph, GraphUndirected
 import time
 
 if __name__ == '__main__':
-    fileName = "graphe.txt"
-    directed_graph = DirectedGraph.load(fileName)
-    print("Voici le graphe avant simplification")
-    print(directed_graph)
-    directed_graph.detected_and_solve_all_cycles()
-    print()
-    print("Voici le graphe après simplification")
-    print(directed_graph)
-    print("Voici les coummunautés:")
-    communities = directed_graph.find_communities()
-    output = ""
-    for community in communities:
-        res =""
-        for node in community:
-            res += node
-        output+= ",".join(res)
-        output+= "\n"
-    print(output)
-    print()
-    print("Voici les hubs sociaux pour des communautés de {} individus".format(3))
-    social_hub = directed_graph.find_social_hub(3)
-    print(social_hub)
-    print()
-    print("Voici le plus grand groupe d'amis")
-    highest_friend_group = directed_graph.find_highest_friend_group()
-    print(highest_friend_group)
+    files = ["graphe.txt", "graphe_2.txt", "graphe_3.txt", "graphe_4.txt"]
+    for file in files:
+        print("--------- Exemple d'exécution pour {} ---------".format(file))
+        directed_graph = DirectedGraph.load(file)
+        print("Voici le graphe avant simplification")
+        print(directed_graph)
+        directed_graph.detect_and_solve_all_cycles()
+        print()
+        print("Voici le graphe après simplification s'il y en a eu")
+        print(directed_graph)
+        print("Voici les coummunautés:")
+        communities = directed_graph.find_communities()
+        output = ""
+        for community in communities:
+            res =""
+            for node in community:
+                res += node
+            output += ",".join(res)
+            output += "\n"
+        print(output)
+        print()
+        highest_friend_group = directed_graph.find_highest_friend_group()
+        print("Voici le plus grand groupe d'amis")
+        print(highest_friend_group)
+        print()
